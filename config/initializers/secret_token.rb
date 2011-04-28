08f2ffb66667891ebbc666dad26334fec3c7c087
Vincent Gittens <vinni101@gmail.com>
2011-04-27 20:32:59 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studiow::Application.config.secret_token = '651b2605eda0b23e00662970443f07c489dd7a43ca1c730cb3a40a3fb10843f0c74b6ebec179be85297bf050092604cb86e144a7a7fde541d1a2494f6c46d9a5'
